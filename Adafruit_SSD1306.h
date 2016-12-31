c20c0e5ed6d29c44959664133866978d1cc71347
Kurt Eckhardt <kurte@rockisland.com>
2016-12-31 07:54:16 -0800

    Allow user to pass in which Wire object to use with display
    Warning again this is using my version of the Wire library that defines
    multiple Wire objects, and you will need to also sync up to changes in
    that branch as with this change I also renamed the base class for my
    Wire objects to be back to the original Wire class, and the Wire object
    now uses TwoWire0 class...

--- Adafruit_SSD1306.h
+++ Adafruit_SSD1306.h
@@ -18,14 +18,12 @@ All text above, and the splash screen must be included in any redistribution
 #ifndef _Adafruit_SSD1306_H_
 #define _Adafruit_SSD1306_H_
 
-#define A1306WIRE Wire
-
 #if ARDUINO >= 100
  #include "Arduino.h"
- #define WIRE_WRITE A1306WIRE.write
+ #define WIRE_WRITE _WireD.write
 #else
  #include "WProgram.h"
-  #define WIRE_WRITE A1306WIRE.send
+  #define WIRE_WRITE _WireD.send
 #endif
 
 #if defined(__SAM3X8E__)
@@ -143,7 +141,8 @@ class Adafruit_SSD1306 : public Adafruit_GFX {
  public:
   Adafruit_SSD1306(int8_t SID, int8_t SCLK, int8_t DC, int8_t RST, int8_t CS);
   Adafruit_SSD1306(int8_t DC, int8_t RST, int8_t CS);
-  Adafruit_SSD1306(int8_t RST = -1);
+
+  Adafruit_SSD1306(int8_t RST = -1, TwoWire &WireD = Wire);
 
   void begin(uint8_t switchvcc = SSD1306_SWITCHCAPVCC, uint8_t i2caddr = SSD1306_I2C_ADDRESS, bool reset=true);
   void ssd1306_command(uint8_t c);
@@ -171,6 +170,7 @@ class Adafruit_SSD1306 : public Adafruit_GFX {
   void fastSPIwrite(uint8_t c);
 
   boolean hwSPI;
+    TwoWire      &_WireD;      // Reference to which Wire Buss object we are using...
 #ifdef HAVE_PORTREG
   PortReg *mosiport, *clkport, *csport, *dcport;
   PortMask mosipinmask, clkpinmask, cspinmask, dcpinmask;
